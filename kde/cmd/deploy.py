4e0c06b11c4bbdeba3a1cb8d4b22497a5be139fe
joe-centos-dev <34218430@qq.com>
2018-02-07 21:54:48 +0800

    modified: fix 1.10,1.11 version of docker check

--- kde/cmd/deploy.py
+++ kde/cmd/deploy.py
@@ -143,8 +143,8 @@ def pre_check(cluster_data, check_leftover):
             node_result["passed"] = "no"
             node_result["details"] = node_result["details"] + "Docker daemon is probably not running. "
         elif "1.12" not in docker_version[0] \
-            or "1.11" not in docker_version[0] \
-            or "1.10" not in docker_version[0]:
+            and "1.11" not in docker_version[0] \
+            and "1.10" not in docker_version[0]:
             node_result["passed"] = "no"
             node_result["details"] = node_result["details"] + "Incompatible docker version; "
 
